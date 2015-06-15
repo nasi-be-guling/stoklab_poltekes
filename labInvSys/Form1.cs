ff0cd6e1e057c664994418033d76b945684e4db9
Putu Widiarsa K <nasi.be.guling@gmail.com>
2015-06-15 09:23:42 +0700

    memulai proyek stok lab - semoga sukses, swaha

---
+++ labInvSys/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using OfficeOpenXml;
+using OfficeOpenXml.Drawing;
+
+/* For I/O purpose */
+using System.IO;
+
+/* For Diagnostics */
+using System.Diagnostics;
+
+namespace labInvSys
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var existingFile = new FileInfo(@"D:\new\Untitled 1.xlsx");
+                // Open and read the XlSX file.
+            using (var package = new ExcelPackage(existingFile))
+            {
+                // Get the work book in the file
+                ExcelWorkbook workBook = package.Workbook;
+                if (workBook != null)
+                {
+                    if (workBook.Worksheets.Count > 0)
+                    {
+                        // Get the first worksheet
+                        ExcelWorksheet currentWorksheet = workBook.Worksheets.First();
+
+                        // read some data
+                        textBox1.Text = currentWorksheet.Cells[1, 1].Text;
+                    }
+                }
+            }
+        }
+
+    }
+}
